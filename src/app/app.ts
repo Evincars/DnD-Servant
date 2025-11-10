fe5adacf8f74a36fce235693128401b8a09618c2
lasakada <adam.lasak@tietoevry.com>
2025-11-10 01:18:57 +0100

    angular-material install

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatButton} from "@angular/material/button";
+import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
 
 @Component({
-  imports: [NxWelcome, RouterModule],
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  template: `
+      <mat-sidenav-container class="container">
+          <mat-sidenav #sidenav mode="over">
+              <p><button matButton (click)="sidenav.toggle()">Toggle</button></p>
+              <p>
+                  test
+              </p>
+          </mat-sidenav>
+
+          <mat-sidenav-content>
+              <p><button matButton (click)="sidenav.toggle()">Toggle</button></p>
+              <p>
+                  test
+              </p>
+          </mat-sidenav-content>
+      </mat-sidenav-container>
+  `,
+  styles: `
+      .container {
+          position: absolute;
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
+      }
+  `,
+    imports: [RouterModule, MatSidenavContainer, MatSidenav, MatButton, MatSidenavContent],
 })
 export class App {
-  protected title = 'DnD-Servant';
 }
