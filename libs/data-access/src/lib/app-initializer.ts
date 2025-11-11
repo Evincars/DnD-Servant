8349723813ce84f02aed58f181db35c23fbbbe2e
lasakada <adam.lasak@tietoevry.com>
2025-11-11 02:46:52 +0100

    firebase init

--- libs/data-access/src/lib/app-initializer.ts
+++ libs/data-access/src/lib/app-initializer.ts
@@ -1,13 +1,29 @@
 import { provideAppInitializer } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
-function initializeApp(): () => Observable<number> {
+function initializeCustomApp(): () => Observable<number> {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBP2KgMhqhfppDZHju6osE6gwtiq1WXFLo',
+    authDomain: 'dnd-servant.firebaseapp.com',
+    projectId: 'dnd-servant',
+    storageBucket: 'dnd-servant.firebasestorage.app',
+    messagingSenderId: '960571423054',
+    appId: '1:960571423054:web:c9e666475047660a0cc61f',
+    measurementId: 'G-WWQG229N8D',
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
   return (): Observable<number> => {
     return of(0);
   };
 }
 
 export const appInitializer = provideAppInitializer(() => {
-  const initializerFn = initializeApp();
+  const initializerFn = initializeCustomApp();
   return initializerFn();
 });
