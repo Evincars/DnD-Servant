1987e18ec505f3cf5d1f9f4ee840882911121747
lasakada <adam.lasak@tietoevry.com>
2025-11-14 00:57:11 +0100

    third page

--- libs/character-sheet/feature/src/lib/third-page.component.ts
+++ libs/character-sheet/feature/src/lib/third-page.component.ts
@@ -3,7 +3,9 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import {
   HeaderInfoForm,
   LookAndFeelForm,
+  ProfessionForm,
   SecondPageForm,
+  SpellsForm,
   ThirdPageForm,
   TopInfoForSpellSheetForm,
 } from '@dn-d-servant/character-sheet-util';
@@ -49,6 +51,301 @@ import {
       class="field checkbox third-page-checkbox"
       style="top:122px; left:1185px;"
     />
+
+    <input
+      [formControl]="controls.topInfoForSpellSheet.controls.mystickyTaj"
+      class="field"
+      style="top:181px; left:960px; width:82px;"
+      placeholder="Mystický taj"
+    />
+    <input
+      [formControl]="controls.topInfoForSpellSheet.controls.mystickyTajCheckbox1"
+      type="checkbox"
+      class="field checkbox third-page-checkbox"
+      style="top:216px; left:1061px;"
+    />
+    <input
+      [formControl]="controls.topInfoForSpellSheet.controls.mystickyTajCheckbox2"
+      type="checkbox"
+      class="field checkbox third-page-checkbox"
+      style="top:216px; left:1103px;"
+    />
+    <input
+      [formControl]="controls.topInfoForSpellSheet.controls.mystickyTajCheckbox3"
+      type="checkbox"
+      class="field checkbox third-page-checkbox"
+      style="top:216px; left:1144px;"
+    />
+    <input
+      [formControl]="controls.topInfoForSpellSheet.controls.mystickyTajCheckbox4"
+      type="checkbox"
+      class="field checkbox third-page-checkbox"
+      style="top:216px; left:1185px;"
+    />
+
+    <input
+      [formControl]="controls.professionForm.controls.prvniPovolaniSesilatelskePovolani"
+      class="field"
+      style="top:346px; left:219px; width:189px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.prvniPovolaniUroven"
+      class="field"
+      style="top:346px; left:419px; width:70px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.prvniPovolaniMaxStupenKouzel"
+      class="field"
+      style="top:346px; left:500px; width:179px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.prvniPovolaniSesilaciVlastnost"
+      class="field"
+      style="top:346px; left:689px; width:179px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.prvniPovolaniSOZachrany"
+      class="field"
+      style="top:346px; left:878px; width:179px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.prvniPovolaniUtocnyBonus"
+      class="field"
+      style="top:346px; left:1068px; width:149px;"
+      placeholder="*"
+    />
+
+    <input
+      [formControl]="controls.professionForm.controls.druhePovolaniSesilatelskePovolani"
+      class="field"
+      style="top:385px; left:219px; width:189px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.druhePovolaniUroven"
+      class="field"
+      style="top:385px; left:419px; width:70px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.druhePovolaniMaxStupenKouzel"
+      class="field"
+      style="top:385px; left:500px; width:179px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.druhePovolaniSesilaciVlastnost"
+      class="field"
+      style="top:385px; left:689px; width:179px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.druhePovolaniSOZachrany"
+      class="field"
+      style="top:385px; left:878px; width:179px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.professionForm.controls.druhePovolaniUtocnyBonus"
+      class="field"
+      style="top:385px; left:1068px; width:149px;"
+      placeholder="*"
+    />
+    <!--    ===================================================================================-->
+
+    <!--    ROW 1-->
+    <!--    ###################################################################################-->
+    <input
+      [formControl]="controls.spellsForm.controls.r1P"
+      type="checkbox"
+      class="field checkbox red-checkbox"
+      style="top:512px; left:69px;"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r1S"
+      class="field spell-row"
+      style="top:511px; left:102px; width:41px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r1Nazev"
+      class="field spell-row "
+      style="top:511px; left:152px; width:193px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r1Utok"
+      class="field spell-row"
+      style="top:511px; left:353px; width:78px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r1DobaSesilani"
+      class="field spell-row"
+      style="top:511px; left:439px; width:58px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r1Slozky"
+      class="field spell-row"
+      style="top:511px; left:506px; width:58px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r1Dosah"
+      class="field spell-row"
+      style="top:511px; left:572px; width:77px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r1Trvani"
+      class="field spell-row"
+      style="top:511px; left:658px; width:77px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r1Poznamka"
+      class="field spell-row"
+      style="top:511px; left:744px; width:408px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r1Str"
+      class="field spell-row"
+      style="top:511px; left:1160px; width:60px;"
+      placeholder="*"
+    />
+
+    <!--    ROW 2-->
+    <!--    ###################################################################################-->
+    <input
+      [formControl]="controls.spellsForm.controls.r2P"
+      type="checkbox"
+      class="field checkbox red-checkbox"
+      style="top:548px; left:69px;"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r2S"
+      class="field spell-row"
+      style="top:546px; left:102px; width:41px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r2Nazev"
+      class="field spell-row "
+      style="top:546px; left:152px; width:193px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r2Utok"
+      class="field spell-row"
+      style="top:546px; left:353px; width:78px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r2DobaSesilani"
+      class="field spell-row"
+      style="top:546px; left:439px; width:58px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r2Slozky"
+      class="field spell-row"
+      style="top:546px; left:506px; width:58px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r2Dosah"
+      class="field spell-row"
+      style="top:546px; left:572px; width:77px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r2Trvani"
+      class="field spell-row"
+      style="top:546px; left:658px; width:77px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r2Poznamka"
+      class="field spell-row"
+      style="top:546px; left:744px; width:408px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r2Str"
+      class="field spell-row"
+      style="top:546px; left:1160px; width:60px;"
+      placeholder="*"
+    />
+
+    <!--    ROW 3-->
+    <!--    ###################################################################################-->
+    <input
+      [formControl]="controls.spellsForm.controls.r3P"
+      type="checkbox"
+      class="field checkbox red-checkbox"
+      style="top:583px; left:69px;"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r3S"
+      class="field spell-row"
+      style="top:582px; left:102px; width:41px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r3Nazev"
+      class="field spell-row "
+      style="top:582px; left:152px; width:193px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r3Utok"
+      class="field spell-row"
+      style="top:582px; left:353px; width:78px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r3DobaSesilani"
+      class="field spell-row"
+      style="top:582px; left:439px; width:58px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r3Slozky"
+      class="field spell-row"
+      style="top:582px; left:506px; width:58px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r3Dosah"
+      class="field spell-row"
+      style="top:582px; left:572px; width:77px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r3Trvani"
+      class="field spell-row"
+      style="top:582px; left:658px; width:77px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r3Poznamka"
+      class="field spell-row"
+      style="top:582px; left:744px; width:408px;"
+      placeholder="*"
+    />
+    <input
+      [formControl]="controls.spellsForm.controls.r3Str"
+      class="field spell-row"
+      style="top:582px; left:1160px; width:60px;"
+      placeholder="*"
+    />
   `,
   styleUrl: './character-sheet.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -76,6 +373,132 @@ export class ThirdPageComponent {
         mystickyTajCheckbox2: fb.control(''),
         mystickyTajCheckbox3: fb.control(''),
         mystickyTajCheckbox4: fb.control(''),
+      }),
+      professionForm: fb.group<ProfessionForm>({
+        prvniPovolaniSesilatelskePovolani: fb.control(''),
+        prvniPovolaniUroven: fb.control(''),
+        prvniPovolaniMaxStupenKouzel: fb.control(''),
+        prvniPovolaniSesilaciVlastnost: fb.control(''),
+        prvniPovolaniSOZachrany: fb.control(''),
+        prvniPovolaniUtocnyBonus: fb.control(''),
+
+        druhePovolaniSesilatelskePovolani: fb.control(''),
+        druhePovolaniUroven: fb.control(''),
+        druhePovolaniMaxStupenKouzel: fb.control(''),
+        druhePovolaniSesilaciVlastnost: fb.control(''),
+        druhePovolaniSOZachrany: fb.control(''),
+        druhePovolaniUtocnyBonus: fb.control(''),
+      }),
+      spellsForm: fb.group<SpellsForm>({
+        r1P: fb.control(''),
+        r1S: fb.control(''),
+        r1Nazev: fb.control(''),
+        r1Utok: fb.control(''),
+        r1DobaSesilani: fb.control(''),
+        r1Slozky: fb.control(''),
+        r1Dosah: fb.control(''),
+        r1Trvani: fb.control(''),
+        r1Poznamka: fb.control(''),
+        r1Str: fb.control(''),
+
+        r2P: fb.control(''),
+        r2S: fb.control(''),
+        r2Nazev: fb.control(''),
+        r2Utok: fb.control(''),
+        r2DobaSesilani: fb.control(''),
+        r2Slozky: fb.control(''),
+        r2Dosah: fb.control(''),
+        r2Trvani: fb.control(''),
+        r2Poznamka: fb.control(''),
+        r2Str: fb.control(''),
+
+        r3P: fb.control(''),
+        r3S: fb.control(''),
+        r3Nazev: fb.control(''),
+        r3Utok: fb.control(''),
+        r3DobaSesilani: fb.control(''),
+        r3Slozky: fb.control(''),
+        r3Dosah: fb.control(''),
+        r3Trvani: fb.control(''),
+        r3Poznamka: fb.control(''),
+        r3Str: fb.control(''),
+
+        r4P: fb.control(''),
+        r4S: fb.control(''),
+        r4Nazev: fb.control(''),
+        r4Utok: fb.control(''),
+        r4DobaSesilani: fb.control(''),
+        r4Slozky: fb.control(''),
+        r4Dosah: fb.control(''),
+        r4Trvani: fb.control(''),
+        r4Poznamka: fb.control(''),
+        r4Str: fb.control(''),
+
+        r5P: fb.control(''),
+        r5S: fb.control(''),
+        r5Nazev: fb.control(''),
+        r5Utok: fb.control(''),
+        r5DobaSesilani: fb.control(''),
+        r5Slozky: fb.control(''),
+        r5Dosah: fb.control(''),
+        r5Trvani: fb.control(''),
+        r5Poznamka: fb.control(''),
+        r5Str: fb.control(''),
+
+        r6P: fb.control(''),
+        r6S: fb.control(''),
+        r6Nazev: fb.control(''),
+        r6Utok: fb.control(''),
+        r6DobaSesilani: fb.control(''),
+        r6Slozky: fb.control(''),
+        r6Dosah: fb.control(''),
+        r6Trvani: fb.control(''),
+        r6Poznamka: fb.control(''),
+        r6Str: fb.control(''),
+
+        r7P: fb.control(''),
+        r7S: fb.control(''),
+        r7Nazev: fb.control(''),
+        r7Utok: fb.control(''),
+        r7DobaSesilani: fb.control(''),
+        r7Slozky: fb.control(''),
+        r7Dosah: fb.control(''),
+        r7Trvani: fb.control(''),
+        r7Poznamka: fb.control(''),
+        r7Str: fb.control(''),
+
+        r8P: fb.control(''),
+        r8S: fb.control(''),
+        r8Nazev: fb.control(''),
+        r8Utok: fb.control(''),
+        r8DobaSesilani: fb.control(''),
+        r8Slozky: fb.control(''),
+        r8Dosah: fb.control(''),
+        r8Trvani: fb.control(''),
+        r8Poznamka: fb.control(''),
+        r8Str: fb.control(''),
+
+        r9P: fb.control(''),
+        r9S: fb.control(''),
+        r9Nazev: fb.control(''),
+        r9Utok: fb.control(''),
+        r9DobaSesilani: fb.control(''),
+        r9Slozky: fb.control(''),
+        r9Dosah: fb.control(''),
+        r9Trvani: fb.control(''),
+        r9Poznamka: fb.control(''),
+        r9Str: fb.control(''),
+
+        r10P: fb.control(''),
+        r10S: fb.control(''),
+        r10Nazev: fb.control(''),
+        r10Utok: fb.control(''),
+        r10DobaSesilani: fb.control(''),
+        r10Slozky: fb.control(''),
+        r10Dosah: fb.control(''),
+        r10Trvani: fb.control(''),
+        r10Poznamka: fb.control(''),
+        r10Str: fb.control(''),
       }),
     });
   }
--- libs/character-sheet/util/src/lib/third-page-form.ts
+++ libs/character-sheet/util/src/lib/third-page-form.ts
@@ -2,6 +2,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 
 export type ThirdPageForm = {
   topInfoForSpellSheet: FormGroup<TopInfoForSpellSheetForm>;
+  professionForm: FormGroup<ProfessionForm>;
+  spellsForm: FormGroup<SpellsForm>;
 };
 
 export type TopInfoForSpellSheetForm = {
@@ -18,6 +20,134 @@ export type TopInfoForSpellSheetForm = {
   mystickyTajCheckbox4: FormControl<string | undefined>;
 };
 
+export type ProfessionForm = {
+  prvniPovolaniSesilatelskePovolani: FormControl<string | undefined>;
+  prvniPovolaniUroven: FormControl<string | undefined>;
+  prvniPovolaniMaxStupenKouzel: FormControl<string | undefined>;
+  prvniPovolaniSesilaciVlastnost: FormControl<string | undefined>;
+  prvniPovolaniSOZachrany: FormControl<string | undefined>;
+  prvniPovolaniUtocnyBonus: FormControl<string | undefined>;
+
+  druhePovolaniSesilatelskePovolani: FormControl<string | undefined>;
+  druhePovolaniUroven: FormControl<string | undefined>;
+  druhePovolaniMaxStupenKouzel: FormControl<string | undefined>;
+  druhePovolaniSesilaciVlastnost: FormControl<string | undefined>;
+  druhePovolaniSOZachrany: FormControl<string | undefined>;
+  druhePovolaniUtocnyBonus: FormControl<string | undefined>;
+};
+
+export type SpellsForm = {
+  r1P: FormControl<string | undefined>;
+  r1S: FormControl<string | undefined>;
+  r1Nazev: FormControl<string | undefined>;
+  r1Utok: FormControl<string | undefined>;
+  r1DobaSesilani: FormControl<string | undefined>;
+  r1Slozky: FormControl<string | undefined>;
+  r1Dosah: FormControl<string | undefined>;
+  r1Trvani: FormControl<string | undefined>;
+  r1Poznamka: FormControl<string | undefined>;
+  r1Str: FormControl<string | undefined>;
+
+  r2P: FormControl<string | undefined>;
+  r2S: FormControl<string | undefined>;
+  r2Nazev: FormControl<string | undefined>;
+  r2Utok: FormControl<string | undefined>;
+  r2DobaSesilani: FormControl<string | undefined>;
+  r2Slozky: FormControl<string | undefined>;
+  r2Dosah: FormControl<string | undefined>;
+  r2Trvani: FormControl<string | undefined>;
+  r2Poznamka: FormControl<string | undefined>;
+  r2Str: FormControl<string | undefined>;
+
+  r3P: FormControl<string | undefined>;
+  r3S: FormControl<string | undefined>;
+  r3Nazev: FormControl<string | undefined>;
+  r3Utok: FormControl<string | undefined>;
+  r3DobaSesilani: FormControl<string | undefined>;
+  r3Slozky: FormControl<string | undefined>;
+  r3Dosah: FormControl<string | undefined>;
+  r3Trvani: FormControl<string | undefined>;
+  r3Poznamka: FormControl<string | undefined>;
+  r3Str: FormControl<string | undefined>;
+
+  r4P: FormControl<string | undefined>;
+  r4S: FormControl<string | undefined>;
+  r4Nazev: FormControl<string | undefined>;
+  r4Utok: FormControl<string | undefined>;
+  r4DobaSesilani: FormControl<string | undefined>;
+  r4Slozky: FormControl<string | undefined>;
+  r4Dosah: FormControl<string | undefined>;
+  r4Trvani: FormControl<string | undefined>;
+  r4Poznamka: FormControl<string | undefined>;
+  r4Str: FormControl<string | undefined>;
+
+  r5P: FormControl<string | undefined>;
+  r5S: FormControl<string | undefined>;
+  r5Nazev: FormControl<string | undefined>;
+  r5Utok: FormControl<string | undefined>;
+  r5DobaSesilani: FormControl<string | undefined>;
+  r5Slozky: FormControl<string | undefined>;
+  r5Dosah: FormControl<string | undefined>;
+  r5Trvani: FormControl<string | undefined>;
+  r5Poznamka: FormControl<string | undefined>;
+  r5Str: FormControl<string | undefined>;
+
+  r6P: FormControl<string | undefined>;
+  r6S: FormControl<string | undefined>;
+  r6Nazev: FormControl<string | undefined>;
+  r6Utok: FormControl<string | undefined>;
+  r6DobaSesilani: FormControl<string | undefined>;
+  r6Slozky: FormControl<string | undefined>;
+  r6Dosah: FormControl<string | undefined>;
+  r6Trvani: FormControl<string | undefined>;
+  r6Poznamka: FormControl<string | undefined>;
+  r6Str: FormControl<string | undefined>;
+
+  r7P: FormControl<string | undefined>;
+  r7S: FormControl<string | undefined>;
+  r7Nazev: FormControl<string | undefined>;
+  r7Utok: FormControl<string | undefined>;
+  r7DobaSesilani: FormControl<string | undefined>;
+  r7Slozky: FormControl<string | undefined>;
+  r7Dosah: FormControl<string | undefined>;
+  r7Trvani: FormControl<string | undefined>;
+  r7Poznamka: FormControl<string | undefined>;
+  r7Str: FormControl<string | undefined>;
+
+  r8P: FormControl<string | undefined>;
+  r8S: FormControl<string | undefined>;
+  r8Nazev: FormControl<string | undefined>;
+  r8Utok: FormControl<string | undefined>;
+  r8DobaSesilani: FormControl<string | undefined>;
+  r8Slozky: FormControl<string | undefined>;
+  r8Dosah: FormControl<string | undefined>;
+  r8Trvani: FormControl<string | undefined>;
+  r8Poznamka: FormControl<string | undefined>;
+  r8Str: FormControl<string | undefined>;
+
+  r9P: FormControl<string | undefined>;
+  r9S: FormControl<string | undefined>;
+  r9Nazev: FormControl<string | undefined>;
+  r9Utok: FormControl<string | undefined>;
+  r9DobaSesilani: FormControl<string | undefined>;
+  r9Slozky: FormControl<string | undefined>;
+  r9Dosah: FormControl<string | undefined>;
+  r9Trvani: FormControl<string | undefined>;
+  r9Poznamka: FormControl<string | undefined>;
+  r9Str: FormControl<string | undefined>;
+
+  r10P: FormControl<string | undefined>;
+  r10S: FormControl<string | undefined>;
+  r10Nazev: FormControl<string | undefined>;
+  r10Utok: FormControl<string | undefined>;
+  r10DobaSesilani: FormControl<string | undefined>;
+  r10Slozky: FormControl<string | undefined>;
+  r10Dosah: FormControl<string | undefined>;
+  r10Trvani: FormControl<string | undefined>;
+  r10Poznamka: FormControl<string | undefined>;
+  r10Str: FormControl<string | undefined>;
+};
+
 export type ThirdPageApiModel = {
   topInfoForSpellSheet: {
     jmenoPostavy: string;
@@ -31,5 +161,131 @@ export type ThirdPageApiModel = {
     mystickyTajCheckbox2: string;
     mystickyTajCheckbox3: string;
     mystickyTajCheckbox4: string;
+  };
+  professionForm: {
+    prvniPovolaniSesilatelskePovolani: string;
+    prvniPovolaniUroven: string;
+    prvniPovolaniMaxStupenKouzel: string;
+    prvniPovolaniSesilaciVlastnost: string;
+    prvniPovolaniSOZachrany: string;
+    prvniPovolaniUtocnyBonus: string;
+
+    druhePovolaniSesilatelskePovolani: string;
+    druhePovolaniUroven: string;
+    druhePovolaniMaxStupenKouzel: string;
+    druhePovolaniSesilaciVlastnost: string;
+    druhePovolaniSOZachrany: string;
+    druhePovolaniUtocnyBonus: string;
+  };
+  spellsForm: {
+    r1P: string;
+    r1S: string;
+    r1Nazev: string;
+    r1Utok: string;
+    r1DobaSesilani: string;
+    r1Slozky: string;
+    r1Dosah: string;
+    r1Trvani: string;
+    r1Poznamka: string;
+    r1Str: string;
+
+    r2P: string;
+    r2S: string;
+    r2Nazev: string;
+    r2Utok: string;
+    r2DobaSesilani: string;
+    r2Slozky: string;
+    r2Dosah: string;
+    r2Trvani: string;
+    r2Poznamka: string;
+    r2Str: string;
+
+    r3P: string;
+    r3S: string;
+    r3Nazev: string;
+    r3Utok: string;
+    r3DobaSesilani: string;
+    r3Slozky: string;
+    r3Dosah: string;
+    r3Trvani: string;
+    r3Poznamka: string;
+    r3Str: string;
+
+    r4P: string;
+    r4S: string;
+    r4Nazev: string;
+    r4Utok: string;
+    r4DobaSesilani: string;
+    r4Slozky: string;
+    r4Dosah: string;
+    r4Trvani: string;
+    r4Poznamka: string;
+    r4Str: string;
+
+    r5P: string;
+    r5S: string;
+    r5Nazev: string;
+    r5Utok: string;
+    r5DobaSesilani: string;
+    r5Slozky: string;
+    r5Dosah: string;
+    r5Trvani: string;
+    r5Poznamka: string;
+    r5Str: string;
+
+    r6P: string;
+    r6S: string;
+    r6Nazev: string;
+    r6Utok: string;
+    r6DobaSesilani: string;
+    r6Slozky: string;
+    r6Dosah: string;
+    r6Trvani: string;
+    r6Poznamka: string;
+    r6Str: string;
+
+    r7P: string;
+    r7S: string;
+    r7Nazev: string;
+    r7Utok: string;
+    r7DobaSesilani: string;
+    r7Slozky: string;
+    r7Dosah: string;
+    r7Trvani: string;
+    r7Poznamka: string;
+    r7Str: string;
+
+    r8P: string;
+    r8S: string;
+    r8Nazev: string;
+    r8Utok: string;
+    r8DobaSesilani: string;
+    r8Slozky: string;
+    r8Dosah: string;
+    r8Trvani: string;
+    r8Poznamka: string;
+    r8Str: string;
+
+    r9P: string;
+    r9S: string;
+    r9Nazev: string;
+    r9Utok: string;
+    r9DobaSesilani: string;
+    r9Slozky: string;
+    r9Dosah: string;
+    r9Trvani: string;
+    r9Poznamka: string;
+    r9Str: string;
+
+    r10P: string;
+    r10S: string;
+    r10Nazev: string;
+    r10Utok: string;
+    r10DobaSesilani: string;
+    r10Slozky: string;
+    r10Dosah: string;
+    r10Trvani: string;
+    r10Poznamka: string;
+    r10Str: string;
   };
 };
